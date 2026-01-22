55aee5474aaaa2f2e7499128c3a2d87b73fefc0d
Angel Risper Ochieng <angelochieng@vt.edu>
2026-01-22 16:44:33 -0500

    Update ProjectRunner.java

--- SpotifyPlaylist/src/dailymixes/ProjectRunner.java
+++ SpotifyPlaylist/src/dailymixes/ProjectRunner.java
@@ -4,13 +4,6 @@ import java.io.FileNotFoundException;
 
 import bsh.ParseException;
 
-//Virginia Tech Honor Code Pledge:
-//Project 4 Spring 2025
-//As a Hokie, I will conduct myself 
-//with honor and integrity at all times.
-//I will not lie, cheat, or steal,
-//nor will I accept the actions of those who do.
-//-- AngelOchieng 906580673
 /**
  * The implementing the song test class
  *
