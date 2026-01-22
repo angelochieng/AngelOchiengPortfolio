fa8b2e8f53d588a90b6767fde0bca46e502fd1f0
Angel Risper Ochieng <angelochieng@vt.edu>
2026-01-22 16:41:15 -0500

    Update Playlist.java

--- SpotifyPlaylist/src/dailymixes/Playlist.java
+++ SpotifyPlaylist/src/dailymixes/Playlist.java
@@ -2,13 +2,6 @@ package dailymixes;
 
 import java.util.Arrays;
 
-//Virginia Tech Honor Code Pledge:
-//Project 4 Spring 2025
-//As a Hokie, I will conduct myself 
-//with honor and integrity at all times.
-//I will not lie, cheat, or steal,
-//nor will I accept the actions of those who do.
-//-- AngelOchieng 906580673
 /**
  * The implementing the playlist class
  *
