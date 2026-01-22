16fa59bde31016347b00dc0f04c35fd9f69fe4ef
Angel Risper Ochieng <angelochieng@vt.edu>
2026-01-22 16:32:33 -0500

    Update DisplayCollection.java

--- DisplayCollection/src/project1/DisplayCollection.java
+++ DisplayCollection/src/project1/DisplayCollection.java
@@ -1,10 +1,5 @@
 //Project 1 2025 Spring
-//Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, 
-//nor will I accept the actions of those who do.
-// -- AngelOchieng (906580673)
+// -- AngelOchieng 
 package project1;
 import bag.Bag;
 import bag.BagInterface; 
