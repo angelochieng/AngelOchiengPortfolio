f9008ddb1f0a29e22daa427a51a583ec49664750
Angel Risper Ochieng <angelochieng@vt.edu>
2026-01-22 16:46:05 -0500

    Update SongTest.java

--- SpotifyPlaylist/src/dailymixes/SongTest.java
+++ SpotifyPlaylist/src/dailymixes/SongTest.java
@@ -2,13 +2,7 @@ package dailymixes;
 
 import student.TestCase;
 
-//Virginia Tech Honor Code Pledge:
-//Project 4 Spring 2025
-//As a Hokie, I will conduct myself 
-//with honor and integrity at all times.
-//I will not lie, cheat, or steal,
-//nor will I accept the actions of those who do.
-//-- AngelOchieng 906580673
+
 /**
  * The implementing the song test class
  *
